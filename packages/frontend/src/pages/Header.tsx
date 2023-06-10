49df97edc50beb00975ac7b81eb90fc0ede7d963
gundeep <gundeep.upenn@gmail.com>
2023-06-10 08:13:10 +0200

    fixed header and added logo

--- packages/frontend/src/pages/Header.tsx
+++ packages/frontend/src/pages/Header.tsx
@@ -6,16 +6,18 @@ export default () => {
     return (
         <>
             <div className="header">
+                <img src={require('../../public/InterviewNinja.svg')} alt="UniRep logo" />
+                <div>powered by</div>
                 <img src={require('../../public/logo.svg')} alt="UniRep logo" />
                 <div className="links">
                     {/* <a href="https://developer.unirep.io/" target="blank">
           Docs
         </a> */}
-                    <span>|</span>
+                    {/* <span>|</span> */}
                     <a href="https://github.com/gundeep/interview-ninja" target="blank">
                         GitHub
                     </a>
-                    <span>|</span>
+                    {/* <span>|</span> */}
                     {/* <a href="https://discord.com/invite/VzMMDJmYc5" target="blank">
           Discord
         </a> */}
